08e36a004a5541e8a37e46e37716ccbf8a227e33
efweating <56888666+efweating@users.noreply.github.com>
2021-08-11 10:47:58 -0400

    Clean up files

--- googlemaps.py
+++
@@ -1,9 +0,0 @@
-import googlemaps
-
-gmaps = googlemaps.Client(key='AIzaSyAPgyovHpbxD2Tk0a0wCQ0dZw8EkRzlEZg')
-
-geocode_result = gmaps.geocode('New York City')
-print(geocode_result[0]["geometry"]["location"])
-
-reverse_geocode_result = gmaps.reverse_geocode((40.574250, -74.006689))
-print(reverse_geocode_result[1]["formatted_address"])
