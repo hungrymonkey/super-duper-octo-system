6fcd3a9554234f1465c75828c949be332ee358da
efweating <56888666+efweating@users.noreply.github.com>
2021-08-12 12:19:18 -0400

    Updated Latitude and Longitude

--- API/cruiselocation.py
+++ API/cruiselocation.py
@@ -2,6 +2,9 @@ import gmplot
 import math
 from haversine import hd
 
+latitude = [40.574250, 44.646855, 50.885678, 38.709906, 34.18671199927991, 25.684660]
+
+longitude = [-74.006689, -63.571072, -1.396664, -9.126695, -77.80923168683415, -80.166976]
 
 gmap = gmplot.GoogleMapPlotter(40.574250, -74.006689,15)
 
@@ -23,23 +26,23 @@ l5 = (34.18671199927991, -77.80923168683415)
 #Miami, FL
 l6 = (25.684660, -80.166976)
 
-lats = [l1[0],l2[0],l3[0],l4[0],l5[0],l6[0]]
-lons = [l1[1],l2[1],l3[1],l4[1],l5[1],l6[1]]
+#lats = [l1[0],l2[0],l3[0],l4[0],l5[0],l6[0]]
+#lons = [l1[1],l2[1],l3[1],l4[1],l5[1],l6[1]]
 
 
 
-gmap.scatter(lats,lons,'red',size=30,marker=False)
+gmap.scatter(latitude,longitude,'red',size=30,marker=False)
 
 min = 99999999
 mini=0
 minj=0
-for i in range(0, len(lats)-1):
-    for j in range(i + 1, len(lats)):
-        if(hd((lats[i],lons[i]),(lats[j],lons[j]))<min):
-            min = hd((lats[i],lons[i]),(lats[j],lons[j]))
+for i in range(0, len(latitude)-1):
+    for j in range(i + 1, len(latitude)):
+        if(hd((latitude[i],longitude[i]),(latitude[j],longitude[j]))<min):
+            min = hd((latitude[i],longitude[i]),(latitude[j],longitude[j]))
             mini=i
             minj=j
 
-gmap.plot([lats[mini],lats[minj]],[lons[mini],lons[minj]],'cornflowerblue',size=30,marker=False)
+gmap.plot([latitude[mini],latitude[minj]],[longitude[mini],longitude[minj]],'cornflowerblue',size=30,marker=False)
 
 gmap.draw("cruisemap.html")
